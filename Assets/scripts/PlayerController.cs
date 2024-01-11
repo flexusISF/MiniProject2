a692551542143ee6e5682ee19ea895bf9760b54c
flexusISF <146860748+flexusISF@users.noreply.github.com>
2024-01-11 10:03:12 -0800

    day 4

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -1,9 +1,13 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
+    public float moveSpeed;
+    public float verticalInput;
     [Header("Turning")]
     public float turnSpeed;
     public float horizontalInput;
@@ -11,20 +15,27 @@ public class PlayerController : MonoBehaviour
     public GameObject projectile;
     public GameObject bulletSpawn;
 
+    //Animation
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        //MOVE 
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.forward * moveSpeed * Time.deltaTime * verticalInput);
+        animator.SetFloat("verticalInput",Mathf.Abs(verticalInput));
         //TURN
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
         //SHOOT
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Q))
         {
             Instantiate(projectile, bulletSpawn.transform.position, bulletSpawn.transform.rotation);
         }
